a1ef13bb2a4ad33f5c5ab7a449b285c0ae9f5069
Andrey <71221128+Test-Tasks-profile@users.noreply.github.com>
2020-09-14 11:11:40 +0300

    Add files via upload

---
+++ precache-manifest.53e14e8b8991662ff913d32a7973e075.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf18e3740dfeacb8320c632eb930e4fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c197b5d35bf2d43a93",
+    "url": "/static/css/main.23b47ba5.chunk.css"
+  },
+  {
+    "revision": "7fb6e64ea528bbaaeb4b",
+    "url": "/static/js/2.7f3b4f1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f3b4f1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c197b5d35bf2d43a93",
+    "url": "/static/js/main.e437af46.chunk.js"
+  },
+  {
+    "revision": "3c723e48decc6f6f72c1",
+    "url": "/static/js/runtime-main.30aace72.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/static/media/menu.6f6bef28.svg"
+  }
+]);
